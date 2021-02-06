bbaf4010189c797eccb5cb948110ec793e30a5f7
René Mundt <remun@dfds.com>
2021-02-06 17:53:23 +0100

    Put inside if block

--- home/scripts.js
+++ home/scripts.js
@@ -1,12 +1,13 @@
 const nameChangeHandler = (value) => {
-  if (value.toLowerCase() === 'renemundt')
-    var inputElement = document.querySelector('input')
-  inputElement.style.display = 'none'
-  var labelElement = document.querySelector('label')
-  labelElement.style.display = 'inline'
-  setTimeout(() => {
-    labelElement.style.display = 'none'
-    var imageElement = document.querySelector('.link-wrapper')
-    imageElement.style.display = 'flex'
-  }, 2000)
+  var inputElement = document.querySelector('input')
+  if (value.toLowerCase() === 'renemundt') {
+    inputElement.style.display = 'none'
+    var labelElement = document.querySelector('label')
+    labelElement.style.display = 'inline'
+    setTimeout(() => {
+      labelElement.style.display = 'none'
+      var imageElement = document.querySelector('.link-wrapper')
+      imageElement.style.display = 'flex'
+    }, 2000)
+  }
 }
