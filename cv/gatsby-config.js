362e0bebf332f684e3aaf3b6dbb45dbece296971
René Mundt <remun@dfds.com>
2021-02-06 18:14:07 +0100

    Add gatsby CV initial sln

---
+++ cv/gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	siteMetadata: {
+		title: `René Mundt - CV`,
+		description: `Curriculum Vitae of René Mundt`,
+		author: `René Mundt`
+	},
+	plugins: [
+		`gatsby-plugin-eslint`,
+		`gatsby-plugin-typescript`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/rene-mundt-icon.png`
+			}
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	]
+}
---
+++ cv/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import SEO from "../components/seo"
+
+const IndexPage: React.FC = () => (
+	<>
+		<SEO title="CV" />
+		<h1>Curriculum Vitae</h1>
+		<p>Personal information</p>
+		<p>..,.</p>
+	</>
+)
+
+export default IndexPage
