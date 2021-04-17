3c344ed490e2b179fe4f4ac1eb30b6c1d218b434
René Mundt <remun@dfds.com>
2021-04-17 22:22:57 +0200

    fix job experience

--- cv/src/components/job-experience.tsx
+++ cv/src/components/job-experience.tsx
@@ -1,32 +1,64 @@
+import styled from "@emotion/styled"
 import * as React from "react"
-import { Col, Row } from "./grid-system"
 import { Heading3 } from "./shared"
+
+const Container = styled.div`
+	display: flex;
+	margin: 1rem;
+	justify-content: space-between;
+	@media (max-width: 768px) {
+		flex: 1;
+		flex-direction: column;
+	}
+`
+
+const JobInfo = styled.div`
+	display: flex;
+	justify-content: space-between;
+	min-width: 500px;
+	@media (max-width: 768px) {
+		min-width: unset;
+	}
+`
+
+const Period = styled.div`
+	min-width: 150px;
+	@media (max-width: 480px) {
+		min-width: 75px;
+	}
+`
 
 const JobExperience: React.FC = () => {
 	return (
-		<Row>
-			<Col size={1}>
-				<Heading3>Job experience</Heading3>
-			</Col>
-			<Col size={1}>
-				<div>System Developer / Consultant - Ementor</div>
-				<div>System Developer Novo Nordisk</div>
-				<div>Consultant / programmer - Region Sjælland</div>
-				<div>Full Stack Developer - Coop Danmark</div>
-				<div>Full Stack Developer - Thomas Cook Airlines</div>
-				<div>Lead Developer / Scrum Master - Thomas Cook Airlines</div>
-				<div>Full stack / Frontend developer - DFDS</div>
-			</Col>
-			<Col size={1}>
-				<div>2000-2002</div>
-				<div>2002-2004</div>
-				<div>2004-2010</div>
-				<div>2010-2012</div>
-				<div>2012-2016</div>
-				<div>2013-2016</div>
-				<div>2016-....</div>
-			</Col>
-		</Row>
+		<Container>
+			<Heading3>Job experience</Heading3>
+			<div>
+				<JobInfo>
+					<div>System Developer / Consultant - Ementor</div>
+					<Period>2000-2002</Period>
+				</JobInfo>
+				<JobInfo>
+					<div>System Developer Novo Nordisk</div>
+					<Period>2002-2004</Period>
+				</JobInfo>
+				<JobInfo>
+					<div>Systen developer - Region Sjælland</div>
+					<Period>2004-2010</Period>
+				</JobInfo>
+				<JobInfo>
+					<div>Full Stack Developer - Coop Danmark</div>
+					<Period>2010-2012</Period>
+				</JobInfo>
+				<JobInfo>
+					<div>Full Stack Developer - Thomas Cook Airlines</div>
+					<Period>2012-2016</Period>
+				</JobInfo>
+				<JobInfo>
+					<div>Full Stack / Frontend developer - DFDS</div>
+					<Period>2016-....</Period>
+				</JobInfo>
+			</div>
+		</Container>
 	)
 }
 
