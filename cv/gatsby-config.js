b9f0b46954c7711b30a22824c86a1dfe831e71f0
René Mundt <remun@dfds.com>
2021-05-02 00:16:57 +0200

    Update packages and make it work css

--- cv/gatsby-config.js
+++ cv/gatsby-config.js
@@ -2,18 +2,19 @@ module.exports = {
 	siteMetadata: {
 		title: `René Mundt - CV`,
 		description: `Curriculum Vitae of René Mundt`,
-		author: `René Mundt`
+		author: `René Mundt`,
 	},
 	plugins: [
 		`gatsby-plugin-eslint`,
 		`gatsby-plugin-typescript`,
 		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-emotion`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `images`,
-				path: `${__dirname}/src/images`
-			}
+				path: `${__dirname}/src/images`,
+			},
 		},
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
@@ -26,11 +27,11 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/rene-mundt-icon.png`
-			}
-		}
+				icon: `src/images/rene-mundt-icon.png`,
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
-	]
+	],
 }
