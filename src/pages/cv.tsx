c630657adc18ee2652337045bd8cc48eb230b87b
René Mundt <remun@dfds.com>
2021-09-23 14:03:35 +0200

    Move cv stuff into cv folder

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,13 +1,26 @@
 import * as React from "react"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+import { Grid } from "../components/grid-system"
+import PersonalInfo from "../components/personal-info"
+import Education from "../components/education"
+import JobExperience from "../components/job-experience"
+import Languages from "../components/languages"
+import Header from "../components/header"
+import Experience from "../components/experience"
 
-const IndexPage: React.FC = () => (
-	<>
+const CvPage: React.FC = () => (
+	<Layout>
 		<SEO title="CV" />
-		<h1>Curriculum Vitae</h1>
-		<p>Personal information</p>
-		<p>...</p>
-	</>
+		<Grid>
+			<Header />
+			<PersonalInfo />
+			<Education />
+			<JobExperience />
+			<Experience />
+			<Languages />
+		</Grid>
+	</Layout>
 )
 
-export default IndexPage
+export default CvPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,11 @@
 import * as React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
-import { Grid } from "../components/grid-system"
-import PersonalInfo from "../components/personal-info"
-import Education from "../components/education"
-import JobExperience from "../components/job-experience"
-import Languages from "../components/languages"
-import Header from "../components/header"
-import Experience from "../components/experience"
 
 const IndexPage: React.FC = () => (
 	<Layout>
-		<SEO title="CV" />
-		<Grid>
-			<Header />
-			<PersonalInfo />
-			<Education />
-			<JobExperience />
-			<Experience />
-			<Languages />
-		</Grid>
+		<SEO title="Home" />
+		René Mundt
 	</Layout>
 )
 
