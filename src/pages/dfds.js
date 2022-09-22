795ed6f556c59c3592203e32c6704d34864f9cb0
René Mundt <remun@dfds.com>
2022-09-22 17:31:24 +0200

    redirect to booking page

--- src/pages/dfds.js
+++ src/pages/dfds.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 const cv = () => {
   useEffect(() => {
     setTimeout(() => {
-      window.location.href = 'https://www.test.dfds.com'
+      window.location.href = 'https://www.test.dfds.com/en-gb/passenger-ferries/booking'
     }, 2000)
   }, [])
 
